c4c5fa778af575864d1f3ed460aab1ef7ee5ab32
NotStealingCode <105090266+NotStealingCode@users.noreply.github.com>
2022-07-11 18:53:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 def caesar(start_text, shift_amount, cipher_direction):
   end_text = ""
-  while shift_amount > 26:
-      shift_amount -= 26
-      
+  
+
   if cipher_direction == "decode":
     shift_amount *= -1
       
@@ -20,14 +19,15 @@ def caesar(start_text, shift_amount, cipher_direction):
   print(f"\nHere's the {cipher_direction}d result: {end_text}")
 
 print(logo)
-restart_program = True
-while restart_program == True:
+running = True
+while running == True:
     direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("\nType your message:\n").lower()
     shift = int(input("\nType the shift number:\n"))
+    shift = shift % 26
     caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
     result = input("\nType 'yes' if you wish to go again otherwise type 'no' to end the program.\n").lower()
     if result == "yes":
-        restart_program = True
+        running = True
     else:
-        restart_program = False
+        running = False
